e95f31d568cbe6d74e0e3a6bc3777ec819e80cc3
islamsoliamn1 <islamelayat38@gmail.com>
2026-02-03 21:20:12 +0200

    EditViews

---
+++ Booking/BLTripster/ViewModels/BookingSuccessVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WebTripster.ViewModels
+{
+    public class BookingSuccessVM
+    {
+       
+        public string HotelName { get; set; } = default!;
+        public string HotelAddress { get; set; } = default!;
+        public int HotelStars { get; set; } = 3;
+
+
+        public string RoomType { get; set; } = default!;
+        public string MainImageUrl { get; set; } = default!;
+
+     
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+   
+        public string CheckInLongDate => CheckIn.ToString("dddd, dd MMMM yyyy");
+        public string CheckOutLongDate => CheckOut.ToString("dddd, dd MMMM yyyy");
+
+   
+        public decimal TotalPrice { get; set; }
+    }
+}
