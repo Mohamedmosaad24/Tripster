ce524d520194e5f8f87b0dd6535f69de4af51c12
a7medreda01 <a7med.reda01@gmail.com>
2026-02-05 17:56:38 +0200

    Ahmed REda Add dashboardLayout

---
+++ Booking/PLTripster/Controllers/NewFolder/Dashboard.cs
@@ -0,0 +1,43 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CenterSystem.Controllers
+{
+    public class Dashboard : Controller
+    {
+        #region Home dashboard
+        //Display analysis
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Hotel dashboard
+        //HotelRepo >> implemntation..getall/edit/add/getid/delete
+
+        #endregion
+
+        #region Rooms dashboard
+        //RoomsRepo >> implemntation..getall/edit/add/getid/delete
+
+        #endregion
+
+        #region Bookings dashboard
+        //BookingsRepo >> implemntation  .. Mangement {update}
+
+        #endregion
+
+        #region Reviews dashboard
+        //ReviewsRepo >> GetAll/RemoveReview
+
+        #endregion
+
+        #region Users dashboard
+        //UsersRepo >> GetAll /RemoveUser/ActiveAccount
+
+        #endregion
+    }
+}
