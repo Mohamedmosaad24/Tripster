13dea83f992d3bf3eebe30c9c2dc22f594302b84
islamsoliamn1 <islamelayat38@gmail.com>
2026-02-11 01:06:49 +0200

    sddingRoomDash

--- Booking/PLTripster/Controllers/Dashboard/Dashboard.cs
+++ Booking/PLTripster/Controllers/Dashboard/Dashboard.cs
@@ -1,10 +1,11 @@
 ﻿
 
 using BLTripster.IServices;
+using BLTripster.Mapping;
+using BLTripster.Services;
 using DALTripster.IRepos;
 using DATripster.Entities;
 using Microsoft.AspNetCore.Mvc;
-using BLTripster.Mapping;
 using System.ComponentModel.DataAnnotations;
 
 namespace DALTripster.Controllers
@@ -12,14 +13,16 @@ namespace DALTripster.Controllers
     public class Dashboard : Controller
     {
         private readonly IHotelService hotelService;
-        private readonly IRepo<Room> _roomRepo;
+       
         private readonly IReviewService reviewService;
 
-        public Dashboard(IHotelService hotelService, IRepo<Room> roomRepo, IReviewService reviewService)
+        private readonly IRoomService _roomService;
+        public Dashboard(IHotelService hotelService, IReviewService reviewService,IRoomService roomService)
         {   
             this.hotelService = hotelService;
-            _roomRepo = roomRepo;
+           
             this.reviewService = reviewService;
+            _roomService = roomService;
         }
         #region Home dashboard
         //Display analysis
@@ -50,8 +53,66 @@ namespace DALTripster.Controllers
 
 
         #region Rooms dashboard
- 
 
+        // LIST ROOMS
+        public IActionResult Rooms()
+        {
+            var rooms = _roomService.GetAll();
+            return View(rooms);
+        }
+
+        // ADD ROOM (GET)
+        [HttpGet]
+        public IActionResult AddRoom()
+        {
+            return View();
+        }
+
+        // ADD ROOM (POST)
+        [HttpPost]
+        public IActionResult AddRoom(Room room)
+        {
+            if (!ModelState.IsValid)
+                return View(room);
+
+            _roomService.Add(room);
+            _roomService.Save();
+
+            return RedirectToAction(nameof(Rooms));
+        }
+
+        // EDIT ROOM (GET)
+        [HttpGet]
+        public IActionResult EditRoom(int id)
+        {
+            var room = _roomService.GetById(id);
+            if (room == null)
+                return NotFound();
+
+            return View(room);
+        }
+
+        // EDIT ROOM (POST)
+        [HttpPost]
+        public IActionResult EditRoom(Room room)
+        {
+            if (!ModelState.IsValid)
+                return View(room);
+
+            _roomService.Update(room);
+            _roomService.Save();
+
+            return RedirectToAction(nameof(Rooms));
+        }
+
+        // DELETE ROOM
+        public IActionResult DeleteRoom(int id)
+        {
+            _roomService.Delete(id);
+            _roomService.Save();
+
+            return RedirectToAction(nameof(Rooms));
+        }
 
         #endregion
 
